30bcea41bb5802d54938477edd6ed246757801f4
Camila <caahmenezes97@gmail.com>
2024-11-07 22:16:27 -0300

    controle da lamp

--- app.js
+++ app.js
@@ -3,7 +3,13 @@
  *  @author Camila
  */
 
-function quebrar() {
+// variaveis de apoio lógica
+let chave = false
+let lampada = true // a lampada está ok 
+
+
+function quebrar(){
+    if (lampada === true) {}
     document.getElementById('lamp').src="img/broken.jpg"
     // reproduzindo um arquivo de áudio no JS
     // Passo 1: copiar o arquivo de audio para o projeto
@@ -11,4 +17,23 @@ function quebrar() {
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    // apoio a lógica para o JS indentificar a lampada quebrada
+    lampada = false
+}
+
+function onoff() {
+    if (chave === false && lampada === true) {
+        // ligar a chave
+        document.getElementById('interruptor').src="img/swon.png"
+        chave = true // o JS agora sabr que a chave está ligada
+        // acender a lampada
+        document.getElementById('lamp').src="img/on.jpg"
+    } else {
+        document.getElementById('interruptor').src="img/swoff.png"
+        chave = false
+        // desligar a lampada
+        document.getElementById('lamp').src="img/off.jpg"
+
+    }
+
 }
